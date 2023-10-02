e1292ba52c8ad57c94fa6cfa5a7a8f99542a27a9
Mahyar Shakouri <shakourimahyar@outlook.com>
2023-10-02 16:02:59 +0330

    pr added

--- Habit21/Utils/TableViewExtension.swift
+++ Habit21/Utils/TableViewExtension.swift
@@ -1,28 +1,3 @@
-//
-//  File.swift
-//  Habit21
-//
-//  Created by Mahyar on 5/7/23.
-//
 
-import Foundation
-import UIKit
-
-extension UITableView {
-
-    func reloadWithAnimation() {
-        reloadData()
-        let tableViewHeight = bounds.size.height
-        let cells = visibleCells
-        var delayCounter = 0
-        for cell in cells {
-            cell.transform = CGAffineTransform(translationX: 0, y: tableViewHeight)
-        }
-        for cell in cells {
-            UIView.animate(withDuration: 1.6, delay: 0.08 * Double(delayCounter),usingSpringWithDamping: 0.6, initialSpringVelocity: 0, options: .curveEaseInOut, animations: {
-                cell.transform = CGAffineTransform.identity
-            }, completion: nil)
-            delayCounter += 1
-        }
-    }
-}
+//import
+//import
--- Habit21/View/Slide/Slide.swift
+++ Habit21/View/Slide/Slide.swift
@@ -1,8 +1,2 @@
-import UIKit
 
-class Slide: UIView {
-    
-    @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var descriptionLabel: UILabel!
-}
+//import
